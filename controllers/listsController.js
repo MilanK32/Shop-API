c451722906954d67d3d5ea9b547d4336444a485c
Milan Kozlovacki <m.kozlovacki@mkozlovacki.levi9.com>
2021-06-21 04:07:18 +0200

    item crud as sub-doc

--- controllers/listsController.js
+++ controllers/listsController.js
@@ -3,7 +3,6 @@ const List = require("../models/List");
 const create_list = (req, res) => {
   const list = new List({
     name: req.body.listName,
-    items: req.body.listItems,
     shop: req.body.listShop,
   });
 
@@ -37,18 +36,12 @@ const get_list = (req, res) => {
 const update_list = (req, res) => {
   const id = req.params.id;
   const name = req.body.listName;
-  const items = req.body.listItems;
   const shop = req.body.listShop;
 
   List.findById(id)
     .then((list) => {
       list.name = name;
       list.shop = shop;
-      if (items) {
-        items.forEach((item) => {
-          list.items.push(item);
-        });
-      }
       return list.save();
     })
     .then((result) => {
@@ -67,10 +60,93 @@ const delete_list = (req, res) => {
     .catch((err) => console.log(err));
 };
 
+const create_list_item = (req, res) => {
+  const id = req.params.id;
+  const item = req.body;
+
+  List.findById(id)
+    .then((list) => {
+      list.items.push(item);
+      return list.save();
+    })
+    .then((result) => {
+      res.json("Item created");
+    })
+    .catch((err) => console.log(err));
+};
+
+const get_list_items = (req, res) => {
+  const id = req.params.id;
+
+  List.findById(id)
+    .populate("items.category")
+    .then((list) => {
+      res.json(list.items);
+    })
+    .catch((err) => console.log(err));
+};
+
+const get_list_item = (req, res) => {
+  const listId = req.params.listId;
+  const itemId = req.params.itemId;
+
+  List.findById(listId)
+    .populate("items.category")
+    .then((list) => {
+      let item = list.items.id(itemId);
+      res.json(item);
+    })
+    .catch((err) => console.log(err));
+};
+
+const update_list_item = (req, res) => {
+  const listId = req.params.listId;
+  const itemId = req.params.itemId;
+  const name = req.body.name;
+  const category = req.body.category;
+  const quantity = req.body.quantity;
+
+  List.findById(listId)
+    .then((list) => {
+      let item = list.items.id(itemId);
+      item.name = name;
+      item.category = category;
+      item.quantity = quantity;
+      return list.save();
+    })
+    .then((result) => {
+      res.json("List updated");
+    })
+    .catch((err) => console.log(err));
+};
+
+const delete_list_item = (req, res) => {
+  const listId = req.params.listId;
+  const itemId = req.params.itemId;
+
+  List.findById(listId)
+    .then((list) => {
+      const updatedListItems = list.items.filter((i) => {
+        return i._id.toString() !== itemId.toString();
+      });
+      list.items = updatedListItems;
+      return list.save();
+    })
+    .then((result) => {
+      res.json("Item deleted");
+    })
+    .catch((err) => console.log(err));
+};
+
 module.exports = {
   create_list,
   get_lists,
   get_list,
   update_list,
   delete_list,
+  get_list_items,
+  get_list_item,
+  create_list_item,
+  update_list_item,
+  delete_list_item,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -7,9 +7,14 @@ const categoryController = require("../controllers/caterogiesController");
 // List routes
 router.get("/lists", listsController.get_lists);
 router.get("/lists/:id", listsController.get_list);
+router.get("/lists/:id/items", listsController.get_list_items);
+router.get("/lists/:listId/items/:itemId", listsController.get_list_item);
 router.post("/lists", listsController.create_list);
+router.post("/lists/:id/items", listsController.create_list_item);
 router.put("/lists/:id", listsController.update_list);
+router.put("/lists/:listId/items/:itemId", listsController.update_list_item);
 router.delete("/lists/:id", listsController.delete_list);
+router.delete("/lists/:listId/items/:itemId", listsController.delete_list_item);
 
 // Shop routes
 router.get("/shops", shopController.get_shops);
